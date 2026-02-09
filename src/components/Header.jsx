91430610d090a4b00539793bbed7d343e6240171
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2026-02-09 17:49:14 +0100

    Fix: Adjust max-width of header container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export const Header = () => {
 	return (
 		<header className="p-3 border-b">
-			<div className="mx-auto lg:max-w-5x flex justify-between items-center">
+			<div className="mx-auto max-w-450 flex justify-between items-center">
 				<h1>CV-Maker</h1>
 				<div className="flex flex-wrap gap-3">
 					<button className="cursor-pointer border px-2">Edit</button>
