ab42152e83b3574e00a941541c738388a94738eb
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2026-02-09 17:56:08 +0100

    Refactor: Improve layout and structure of ExperienceForm component

--- src/components/forms/ExperienceForm.jsx
+++ src/components/forms/ExperienceForm.jsx
@@ -2,30 +2,32 @@ export const ExperienceForm = ({ expType, organization }) => {
 	const description = `Write a few sentences about your experience at this organization...`;
 	return (
 		<form>
-			<div>
-				<input type="text" placeholder={expType} />
-				<div className="sm:flex gap-3">
-					<label htmlFor="start-date" className="flex">
-						Start:
-						<input type="date" name="start-date" id="start-date" />
-					</label>
+			<div className="flex flex-col gap-2 p-1.5 border">
+				<div className="sm:flex sm:justify-between">
+					<input type="text" placeholder={expType} />
+					<div className="sm:flex sm:gap-3">
+						<label htmlFor="start-date" className="flex">
+							Start:
+							<input type="date" name="start-date" id="start-date" />
+						</label>
 
-					<label htmlFor="end-date" className="flex">
-						End:
-						<input type="date" name="end-date" id="end-date" />
-					</label>
+						<label htmlFor="end-date" className="flex">
+							End:
+							<input type="date" name="end-date" id="end-date" />
+						</label>
+					</div>
 				</div>
-			</div>
-			<input type="text" placeholder={organization} />
-			<div>
-				<label htmlFor="summary" className="block mb-2">
-					Description
-				</label>
-				<textarea
-					name="summary"
-					id="summary"
-					className="w-full placeholder:text-gray-500"
-					placeholder={description}></textarea>
+				<input type="text" placeholder={organization} />
+				<div>
+					<label htmlFor="description" className="block mb-2">
+						Description
+					</label>
+					<textarea
+						name="description"
+						id="description"
+						className="w-full placeholder:text-gray-500"
+						placeholder={description}></textarea>
+				</div>
 			</div>
 		</form>
 	);
