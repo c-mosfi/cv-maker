af99d3ccf449c18aaef5b7d64cc31935981b70b6
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2026-02-17 11:57:10 +0100

    Refactor: Cv component to implement editing and viewing modes with CvForm and CvView

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -1,23 +1,49 @@
-import { PersonalDetails } from './PersonalDetails';
-import { WorkExp } from './WorkExp';
-import { EducationExp } from './EducationExp';
+import { useState } from 'react';
+import { CvForm } from './form/CvForm';
+import { CvView } from './CvView';
 
 export const Cv = () => {
+	let [isEditing, setIsEditing] = useState(true);
+	let [formData, setFormData] = useState({
+		generalData: {}, // single
+		practicalData: [], // multiple object entries [{exp1},{exp2}]
+		educationalData: [], // multiple object entries [{exp1},{exp2}]
+	});
+
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		// TODO: Get form values with some function
+		const formValues = null;
+		setFormData(formValues);
+
+		setIsEditing(false);
+	}
+
 	return (
 		<>
+			<nav className="mx-auto max-w-5xl p-3 flex justify-center gap-3">
+				{isEditing ? (
+					<button
+						className="cursor-pointer border px-2 hover:bg-blue-300/50"
+						onClick={handleSubmit}>
+						Submit & View
+					</button>
+				) : (
+					<button
+						className="cursor-pointer border px-2 hover:bg-blue-300/50"
+						onClick={() => setIsEditing(true)}>
+						Edit
+					</button>
+				)}
+			</nav>
+
 			<main className="mx-auto max-w-5xl p-3">
-				<div className="flex gap-3 mb-3">
-					<button className="cursor-pointer border px-2">Edit</button>
-					<button className="cursor-pointer border px-2">View</button>
-				</div>
-				<div className="border p-2 flex flex-col gap-3">
-					{/*TODO: 
-					- conditional rendering of Edit/View
-					- add a prop for mode: then inside the component render edit(forms) or view(plain html) */}
-					<PersonalDetails />
-					<WorkExp />
-					<EducationExp />
-				</div>
+				{isEditing ? (
+					<CvForm currentData={formData} onSubmit={handleSubmit} />
+				) : (
+					<CvView currentData={formData} />
+				)}
 			</main>
 		</>
 	);
