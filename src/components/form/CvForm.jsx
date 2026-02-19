3e60777c0e916b6d780eea501d362436e97be992
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2026-02-19 18:51:06 +0100

    Add state management to CvForm for general, practical, and educational data

--- src/components/form/CvForm.jsx
+++ src/components/form/CvForm.jsx
@@ -1,14 +1,27 @@
+import { useState } from 'react';
 import { ExperienceFieldset } from './ExperienceFieldset';
 import { GeneralInfoFieldset } from './GeneralInfoFieldset';
 
 export const CvForm = ({ onSubmit }) => {
+	const [generalData, setGeneralData] = useState({});
+	const [practicalData, setPracticalData] = useState([]);
+	const [educationalData, setEducationalData] = useState([]);
+
 	return (
 		<form className="flex flex-col gap-2" onSubmit={onSubmit}>
 			<h1>CvForm</h1>
 
-			<GeneralInfoFieldset />
-			<ExperienceFieldset type={'practical'} />
-			<ExperienceFieldset type={'education'} />
+			<GeneralInfoFieldset data={generalData} setData={setGeneralData} />
+			<ExperienceFieldset
+				type={'practical'}
+				entries={practicalData}
+				setEntries={setPracticalData}
+			/>
+			<ExperienceFieldset
+				type={'education'}
+				entries={educationalData}
+				setEntries={setEducationalData}
+			/>
 		</form>
 	);
 };
