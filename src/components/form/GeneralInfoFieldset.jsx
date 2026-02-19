7b513155706b3902cfed4dfe074e91b4a10e01d6
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2026-02-19 18:52:48 +0100

    Refactor: GeneralInfoFieldset to manage input state. Update: field names for object keys

--- src/components/form/GeneralInfoFieldset.jsx
+++ src/components/form/GeneralInfoFieldset.jsx
@@ -1,28 +1,41 @@
-export const GeneralInfoFieldset = () => {
+export const GeneralInfoFieldset = ({ data, setData }) => {
+	// state lives in CvForm (parent)
+	function handleChange(e) {
+		setData({
+			...data,
+			[e.target.name]: e.target.value,
+		});
+	}
+
 	return (
 		<fieldset>
 			<h2 className="mb-2">General Info</h2>
 			<div className="flex flex-col gap-2 p-1.5 border">
 				<label>
 					Full name
-					<input type="text" name="full-name" />
+					<input type="text" name="fullName" onChange={handleChange} />
 				</label>
 				<label>
 					Email address
-					<input type="email" name="email-address" />
+					<input type="email" name="emailAddress" onChange={handleChange} />
 				</label>
 				<label>
 					Phone number
-					<input type="text" name="phone-number" />
+					<input type="text" name="phoneNumber" onChange={handleChange} />
 				</label>
 				<label>
 					City
-					<input type="text" name="city-country" />
+					<input type="text" name="city" onChange={handleChange} />
+				</label>
+				<label>
+					Country
+					<input type="text" name="country" onChange={handleChange} />
 				</label>
 				<label htmlFor="summary" className="mb-2">
 					Professional Summary
 					<textarea
 						name="summary"
+						onChange={handleChange}
 						className="w-full placeholder:text-gray-500"
 						placeholder="Write a few sentences about yourself..."></textarea>
 				</label>
