1ff3974c452336b6c6801dc04b01a7500243b378
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2026-02-19 18:54:39 +0100

    Refactor: Enhance ExperienceEntry component to manage entry state and improve input naming conventions

--- src/components/form/ExperienceEntry.jsx
+++ src/components/form/ExperienceEntry.jsx
@@ -1,39 +1,54 @@
-export const ExperienceEntry = ({ type, onDelete }) => {
+export const ExperienceEntry = ({
+	type,
+	entryID,
+	entry,
+	setEntry,
+	onDelete,
+}) => {
 	const isPracticalExp = type === 'practical';
+
+	function handleChange(e) {
+		setEntry({
+			id: entryID,
+			...entry,
+			[e.target.name]: e.target.value,
+		});
+	}
 
 	return (
 		<section className="flex flex-col gap-2 border p-1.5">
 			<div className="sm:flex sm:justify-between">
 				{/* Name of exp */}
-				<label htmlFor="exp-name">
+				<label htmlFor="experienceName">
 					{isPracticalExp ? 'Position:' : 'Title:'}
-					<input type="text" name="exp-name" />
+					<input type="text" name="experienceName" onChange={handleChange} />
 				</label>
 
 				{/* Dates */}
 				<div className="sm:flex sm:gap-3">
-					<label htmlFor="start-date" className="flex">
+					<label htmlFor="startDate" className="flex">
 						Start:
-						<input type="date" name="start-date" />
+						<input type="date" name="startDate" onChange={handleChange} />
 					</label>
-					<label htmlFor="end-date" className="flex">
+					<label htmlFor="endDate" className="flex">
 						End:
-						<input type="date" name="end-date" />
+						<input type="date" name="endDate" onChange={handleChange} />
 					</label>
 				</div>
 			</div>
 
 			{/* Exp place info */}
-			<label htmlFor="exp-place">
+			<label htmlFor="experiencePlace">
 				{isPracticalExp ? 'Company:' : 'Institution:'}
-				<input type="text" name="exp-place" />
+				<input type="text" name="experiencePlace" onChange={handleChange} />
 			</label>
 			<div>
-				<label htmlFor="exp-description" className="mb-2">
+				<label htmlFor="experienceDescription" className="mb-2">
 					Description
 				</label>
 				<textarea
-					name="exp-description"
+					name="experienceDescription"
+					onChange={handleChange}
 					className="w-full placeholder:text-gray-500"
 					placeholder="Write a few sentences about your experience.."></textarea>
 			</div>
